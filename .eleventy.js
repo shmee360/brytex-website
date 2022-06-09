f444a73867edfe23347f09ab80eb42dc1fc3dfcb
Warren <me@warrenstix.xyz>
2022-06-09 17:28:34 -0600

    finished second tutorial

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,11 @@
+const fs = require("fs");
+const filesize = require("file-size");
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("img");
-    eleventyConfig.addLayoutAlias('default', 'layouts/default.njk');
+    eleventyConfig.addLayoutAlias('default', 'layouts/default.liquid');
+    eleventyConfig.addFilter("filesize", (path) => (
+        fs.statSync(path) ?
+            filesize(fs.statSync(path).size).human() :
+            ""));
 };
