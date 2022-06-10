4f432bfee90f9ad02dbb0636fe37f51af1ea5760
Warren <me@warrenstix.xyz>
2022-06-10 15:12:20 -0600

    slideshow semi-done

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,5 @@
-const fs = require("fs");
-const filesize = require("file-size");
-
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("img");
-    eleventyConfig.addLayoutAlias('default', 'layouts/default.liquid');
-    eleventyConfig.addFilter("filesize", (path) => (
-        fs.statSync(path) ?
-            filesize(fs.statSync(path).size).human() :
-            ""));
+    eleventyConfig.addLayoutAlias("default", "layouts/default.liquid");
+    eleventyConfig.addWatchTarget("./_assets/css/");
 };
