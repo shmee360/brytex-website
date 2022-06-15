a9266e84f49798d8ca4f154a58ea3527d97f8c9d
Warren <me@warrenstix.xyz>
2022-06-15 13:46:27 -0600

    made image loading lazy

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
+const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("img");
     eleventyConfig.addLayoutAlias("default", "layouts/default.liquid");
     eleventyConfig.addWatchTarget("./_assets/css/");
     eleventyConfig.addPlugin(EleventyRenderPlugin);
+    eleventyConfig.addPlugin(lazyImagesPlugin);
 };
