6a756bd96df31a0d6dae3904efb1b7d0f28e2e49
Warren <me@warrenstix.xyz>
2022-06-16 11:10:47 -0600

    fade-in effect done

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -2,7 +2,39 @@ import 'bootstrap';
 import './../css/main.scss';
 import './vendor/popover';
 import './vendor/tooltip';
-import $ from 'jquery';
+
+const AnimTime = 2000;
+const FadeClass = "fadein";
+
+let ticking = false;
+
+window.onload = () => fadeInAll(document, FadeClass, AnimTime);
+
+document.addEventListener('scroll', e => {
+    if (!ticking) {
+        window.requestAnimationFrame(() => {
+            fadeInAll(document, FadeClass, AnimTime);
+
+            ticking = false;
+        });
+
+        ticking = true;
+    }
+});
+
+const fadeInAll = (doc, cls, duration) => {
+    [].forEach.call(doc.getElementsByClassName(cls), i => {
+        if (onScreen(i)) {
+            i.style.opacity = 1;
+            i.style.transform = "translateY(0px)";
+
+            // Wait for animation to play before removing class
+            setTimeout(() => {
+                i.classList.remove(cls);
+            }, duration);
+        }
+    });
+}
 
 const onScreen = el => {
     let portHeight =
@@ -10,24 +42,7 @@ const onScreen = el => {
                  window.innerHeight || 0);
 
     let bottom = el.getBoundingClientRect().top +
-        el.getBoundingClientRect().height;
+        el.getBoundingClientRect().height / 2;
 
     return 0 <= bottom && bottom <= portHeight;
-};
-
-const fadeImages = () => $('.fadein').each(function(i) {
-    let bottom_of_element = $(this).offset().top + $(this).outerHeight();
-    let bottom_of_window = $(window).scrollTop() + $(window).height();
-    
-    if (bottom_of_window > bottom_of_element) {
-        $(this).animate({'opacity': '1'}, 1000);
-    }
-});
-
-$(document).ready(() => {
-    fadeImages();
-
-    $(window).scroll(() => {
-        fadeImages();
-    });
-});
+}
