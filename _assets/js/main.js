e962a29fddfbc09cf8c221df06f7aeb0a4b20c81
Warren <me@warrenstix.xyz>
2022-06-17 14:58:46 -0600

    gallery done no modal

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -5,15 +5,20 @@ import './vendor/tooltip';
 
 const AnimTime = 2000;
 const FadeClass = "fadein";
+const SlideClass = "slidein";
 
 let ticking = false;
 
-window.onload = () => fadeInAll(document, FadeClass, AnimTime);
+window.onload = () => {
+    scrollAnimCls(document, FadeClass, AnimTime, fadeInOne);
+    scrollAnimCls(document, SlideClass, 1000, slideInRightOne);
+}
 
 document.addEventListener("scroll", e => {
     if (!ticking) {
         window.requestAnimationFrame(() => {
             scrollAnimCls(document, FadeClass, AnimTime, fadeInOne);
+            scrollAnimCls(document, SlideClass, 1000, slideInRightOne);
 
             ticking = false;
         });
@@ -27,8 +32,14 @@ const scrollAnimCls = (doc, cls, duration, fn) => {
 
     [].forEach.call(doc.getElementsByClassName(cls), el => {
         if (onScreen(el)) {
-            setTimeout(() => fn(el, cls, duration),
-                duration / 12 * i++);
+            setTimeout(() => {
+                fn(el, cls, duration);
+
+                // Wait for animation to play before removing class
+                setTimeout(() => {
+                    el.classList.remove(cls);
+                }, duration);
+            }, duration / 12 * i++);
         }
     });
 }
@@ -36,11 +47,10 @@ const scrollAnimCls = (doc, cls, duration, fn) => {
 const fadeInOne = (el, cls, duration) => {
     el.style.opacity = 1;
     el.style.transform = "translateY(0px)";
+}
 
-    // Wait for animation to play before removing class
-    setTimeout(() => {
-        el.classList.remove(cls);
-    }, duration);
+const slideInRightOne = (el, cls, duration) => {
+    el.style.transform = "translateX(0)";
 }
 
 const onScreen = el => {
