0e13cb0b785ccdc5402a2f164b61f813a347b19a
Warren <me@warrenstix.xyz>
2022-06-17 10:20:40 -0600

    upgraded image fading

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -8,9 +8,11 @@ const FadeClass = "fadein";
 
 let ticking = false;
 
-window.onload = () => fadeInAll(document, FadeClass, AnimTime);
+window.onload = () => {
+    fadeInAll(document, FadeClass, AnimTime);
+}
 
-document.addEventListener('scroll', e => {
+document.addEventListener("scroll", e => {
     if (!ticking) {
         window.requestAnimationFrame(() => {
             fadeInAll(document, FadeClass, AnimTime);
@@ -23,15 +25,19 @@ document.addEventListener('scroll', e => {
 });
 
 const fadeInAll = (doc, cls, duration) => {
-    [].forEach.call(doc.getElementsByClassName(cls), i => {
-        if (onScreen(i)) {
-            i.style.opacity = 1;
-            i.style.transform = "translateY(0px)";
+    let i = 0;
 
-            // Wait for animation to play before removing class
+    [].forEach.call(doc.getElementsByClassName(cls), el => {
+        if (onScreen(el)) {
             setTimeout(() => {
-                i.classList.remove(cls);
-            }, duration);
+                el.style.opacity = 1;
+                el.style.transform = "translateY(0px)";
+
+                // Wait for animation to play before removing class
+                setTimeout(() => {
+                    el.classList.remove(cls);
+                }, duration);
+            }, duration / 12 * i++);
         }
     });
 }
