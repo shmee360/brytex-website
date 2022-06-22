d7633633158cfc229cdf3dcf68a0acf7436e2a28
Warren <me@warrenstix.xyz>
2022-06-22 09:13:05 -0600

    navbar logo ""fixed""

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const { EleventyRenderPlugin } = require("@11ty/eleventy");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("img");
+    eleventyConfig.addPassthroughCopy("logo.9284a574.jpg");
     eleventyConfig.addLayoutAlias("default", "layouts/default.liquid");
     eleventyConfig.addWatchTarget("./_assets/css/");
     eleventyConfig.addWatchTarget("./_assets/js/");
